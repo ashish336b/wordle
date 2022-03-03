7174999106905d26bdaa40a75da7812985ab9640
ashish336b <ashish336b@gmail.com>
2022-03-03 21:19:15 +0545

    command setup

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/ashish336b/wordle-in-terminal
 
 go 1.17
+
+require (
+	github.com/fatih/color v1.13.0
+	github.com/urfave/cli/v2 v2.3.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- main.go
+++ main.go
@@ -2,27 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/ashish336b/wordle-in-terminal/helper"
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
 )
 
-func main() {
+func playGame(c *cli.Context) error {
 	rand.Seed(time.Now().UnixNano())
 	solutionIndex := rand.Intn(len(helper.Words))
 	solutionWord := strings.ToUpper(helper.Words[solutionIndex])
-	solutionWordChars := []rune(solutionWord)
+	solutionWordChars := strings.Split(solutionWord, "")
 	fmt.Println(solutionWordChars)
+	i := 1
 	for {
 		var guess string
+		fmt.Printf("Enter your guess %d/%d: \n", i, 6)
 		fmt.Scanln(&guess)
 		guess = strings.ToUpper(guess)
-		if guess == solutionWord {
-			fmt.Println("You win!")
+		if guess == solutionWord || i == 6 {
+			if i == 6 {
+				fmt.Println("you lose!")
+				break
+			}
+			color.Green("You win!")
 			break
 		}
-		fmt.Println("Nope, try again")
+		i++
+	}
+	return nil
+}
+func main() {
+	err := (&cli.App{
+		Name:   "wordle-go",
+		Usage:  "wordle-in-terminal",
+		Action: playGame,
+		Commands: []*cli.Command{
+			{
+				Name:    "play",
+				Aliases: []string{"p"},
+				Usage:   "play the game",
+				Action: func(c *cli.Context) error {
+					fmt.Println("hello world")
+					return nil
+				},
+			},
+		},
+	}).Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
