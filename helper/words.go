0c807cf9f1916d01682040722ae20c78cdc32901
ashish336b <ashish336b@gmail.com>
2022-03-03 23:15:10 +0545

    check accuracy word for guessed word

--- helper/words.go
+++ helper/words.go
@@ -1,3 +1,57 @@
 package helper
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
 var Words = []string{"Quick", "Brown", "hello", "arose", "vivid"}
+
+func Check() string {
+	a := lipgloss.NewStyle().
+		Bold(true).
+		Background(lipgloss.Color("#14c700")).
+		Render("a")
+	b := lipgloss.NewStyle().
+		Bold(true).
+		Background(lipgloss.Color("#14c700")).
+		Render("b")
+	fmt.Println(a + b)
+	return "hello"
+}
+func CheckAccuracy(solution string, guessed string) string {
+	solutionWordArr := strings.Split(solution, "")
+	guessedWordArr := strings.Split(guessed, "")
+	finalWord := ""
+	style := lipgloss.NewStyle().
+		Bold(true).
+		Inline(true).
+		Padding(1)
+	for i, value := range solutionWordArr {
+		if guessedWordArr[i] == value {
+			finalWord += style.
+				Background(lipgloss.Color("#14c700")).
+				Render(value)
+		}
+		if guessedWordArr[i] != value && include(guessedWordArr[i], solutionWordArr) {
+			finalWord += style.
+				Background(lipgloss.Color("#e6d600")).
+				Render(guessedWordArr[i])
+		}
+		if !include(guessedWordArr[i], solutionWordArr) {
+			finalWord += style.Render(guessedWordArr[i])
+		}
+	}
+	return finalWord
+}
+
+func include(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -9,13 +9,12 @@ import (
 	"time"
 
 	"github.com/ashish336b/wordle-in-terminal/helper"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
 )
 
 func officialWordle(c *cli.Context) error {
-	fmt.Println("official wordle")
+	fmt.Println(helper.CheckAccuracy("hello", "aeipe"))
 	return nil
 }
 func playGame(c *cli.Context) error {
@@ -27,7 +26,7 @@ func playGame(c *cli.Context) error {
 	i := 1
 	for {
 		var guess string
-		fmt.Printf("Enter your guess %d/%d: \n", i, 6)
+		fmt.Printf("Enter your guess %d/%d: \t", i, 6)
 		fmt.Scanln(&guess)
 		guess = strings.ToUpper(guess)
 		if guess == solutionWord || i == 6 {
@@ -39,24 +38,15 @@ func playGame(c *cli.Context) error {
 			break
 		}
 		i++
+		fmt.Println(guess)
 	}
 	return nil
 }
 func main() {
-	var style = lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#FAFAFA")).
-		PaddingBottom(2).
-		PaddingRight(2).
-		PaddingTop(2).
-		PaddingLeft(2).
-		Width(22)
-	fmt.Println(style.Render("hello world"))
-	return
 	err := (&cli.App{
 		Name:   "wordle-go",
 		Usage:  "wordle-in-terminal",
-		Action: playGame,
+		Action: officialWordle,
 		Commands: []*cli.Command{
 			{
 				Name:    "play",
